55b4058f7973533693f37df453c788115a1744ef
Mamdouh_Mansour <mamdoh@terkwaz.com>
2021-08-27 11:55:55 +0200

    Add yml file

--- generate_allure_report.bat
+++ generate_allure_report.bat
@@ -1,5 +1,5 @@
 @echo off
-set path=C:\Users\Mamduh\.m2\repository\allure\allure-2.14.0\bin;C:\Program Files\Java\jdk-15.0.2\bin;%path%
+set path=C:\Users\Mamduh\.m2\repository\allure\allure-2.14.0\bin;D:\PRGRAMMING\eclipse\eclipse-java-2021-03-R-win32-x86_64\eclipse\plugins\org.eclipse.justj.openjdk.hotspot.jre.full.win32.x86_64_15.0.2.v20210201-0955\jre\bin;%path%
 allure serve allure-results
 pause
 exit
